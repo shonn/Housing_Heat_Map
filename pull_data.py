b00fa75f2ee78ad965e0c11abea20ea17af8fb1a
Shawn Nguyen <ssn@xubuntupad.(none)>
2013-09-13 11:39:24 -0700

    added pull functionality

--- pull_data.py
+++ pull_data.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
+import Quandl
+import string
 
 """This script pulls real estate data"""
+
+count = 0
+with open('zipcodes.bak', 'r') as zipfile, open('dataset5.txt', 'w') as datafile:
+  for zipcode in zipfile:
+    zipcode = zipcode.rstrip()
+    if count == 9000:
+      break
+    print zipcode
+    try:
+      data_object = Quandl.get('ZILLOW/ZIP_ALLHOMES_' + zipcode, collapse='annual', rows=17)
+    except:
+      print (zipcode + " skipped.\n")
+      continue
+    count += 1
+    dataset = str(data_object)
+    dataset = string.split(dataset, '\n')
+    for data in dataset:
+      if "Value" in data or "Date" in data:
+        continue
+      datafile.write(zipcode + " " + data + "\n")
+zipfile.close()
+datafile.close()
+
