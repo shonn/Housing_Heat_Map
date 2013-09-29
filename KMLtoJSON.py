a87e615f8f8ef54bc5e1423eb709824f77d5f7d3
Alex Wu <hehahohee@gmail.com>
2013-09-29 14:56:46 -0700

    Renamed reformatKML to KMLtoJSON. fixed a bug were a line will be writen twice in KMLtoJSON Created KMLtoWKT.py which is suppose to create a mysqlimport friendly file that imports geometry type data. doesn't quite work yet

--- KMLtoJSON.py
+++ KMLtoJSON.py
@@ -1,6 +1,7 @@
 '''
 Created on Sep 25, 2013
 Grabs the data we want from the KML and converts it into a format friendly to mysqlimport
+Polygons are defined in JSON arrays
 @author: hehahohee
 '''
 
@@ -12,7 +13,7 @@ kml_file = path.join("/Applications/XAMPP/htdocs/CS157A/HousingPriceHeatMap/zipc
 with open(kml_file) as f:
     doc = parser.parse(f)
 root = doc.getroot()
-outf = open('zipcoordinates.txt','w')
+outf = open('zipJSON.txt','w')
 for placemark in root.Document.Folder.Placemark:
     if(re.match('^\\d+$', placemark.name.text)):
         print placemark.name.text;
@@ -20,7 +21,6 @@ for placemark in root.Document.Folder.Placemark:
         if(hasattr(placemark,"Polygon")):
             jsonShapePath += "[[" + placemark.Polygon.outerBoundaryIs.LinearRing.coordinates.text.replace(",", "],[") +"]]"
             jsonShapePath = jsonShapePath.replace(" ", ",")
-            outf.write(placemark.name.text + '\t' + jsonShapePath +'\n');
         elif(hasattr(placemark, "MultiGeometry")):
             for polygon in placemark.MultiGeometry.Polygon:
                 jsonShapePath += "[[" + polygon.outerBoundaryIs.LinearRing.coordinates.text.replace(",", "],[") +"]]"
